89b78dee5fc5c14e633b1acb9df0a5b09355efb3
sarafchetan <saraf.chetan@gmail.com>
2020-02-12 01:30:39 +0530

    Add files via upload
    New Calculate java files with TDD

--- com-calculator-operation/src/main/java/calculator/CalculateOperation.java
+++ com-calculator-operation/src/main/java/calculator/CalculateOperation.java
@@ -1,19 +1,53 @@
 package calculator;
 
+import java.util.regex.Pattern;
+
 public class CalculateOperation {
 
-	int add(int a, int b) {
-		return a+b;
-	}
+	
+	
+	private final String _VALID_INPUT_REGEX = "(\\d*\\W{1}\\d+)+";
 
-	public int division(int a, int b) {
-		return a/b;
-	}
-
-	public int multiplication(int i, int j) {
+	public int stringadd(String number1) {
 		
-		return i*j;
+		return 0;
 	}
 
+    public int stringaddvalid(String number1, String number2) {
+		
+		return Integer.parseInt(number1) + Integer.parseInt(number2);
+	}
+
+     public int stringaddvalid(String number1) throws Exception {
+		
+		 throw new Exception("Invalid Exception, Please enter data again");
+	}
+
+	public Integer stringaddusingdelimiter(String numbers) throws Exception {
+		int add=0;
+		if(!numbers.isEmpty()) {
+			if(isValidInput(numbers)) {
+				throw new Exception("Invalid Exception, Please enter data again" + numbers);
+			}
+			else {
+				final String finalNumber[]=numbers.split("\\W");
+				System.out.println(finalNumber);
+				for (int i=0;i<finalNumber.length;i++) {
+					add+=Integer.parseInt(finalNumber[i]);
+				}
+			}
+				
+		}
+		
+		
+		return add;
+	}
+
+	private boolean isValidInput(String numbers) {
+		final Pattern pattern = Pattern.compile(_VALID_INPUT_REGEX);
+		return numbers.contains("-") || numbers.contains(".") || !pattern.matcher(numbers).matches();
+		
+	}   
+	
 	
 }
